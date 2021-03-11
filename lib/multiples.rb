37e466ee200bb11ae3322d4c5a450961f67e2e21
Christopher Kim <chris.kim1990@gmail.com>
2021-03-11 09:54:35 -0500

    commit tree 6fa5abec20b44992a0a6ae2e2f1fbc55fb5e8888

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,35 @@
-# Enter your procedural solution here!
+
+# def collect_multiples(limit)
+#     collection = []
+#     (1...limit).each do |num|
+#         if (num % 3 == 0 || num % 5 == 0)
+#         collection << num
+#         end
+#     end
+#     collection
+# end
+
+# def sum_multiples(limit)
+#     collect_multiples(limit).sum
+# end
+
+class Multiples
+    def initialize (limit)
+        @limit = limit
+    end
+
+    def collect_multiples(limit)
+        collection = []
+        (1...limit).each do |num|
+            if (num % 3 == 0 || num % 5 == 0)
+            collection << num
+            end
+        end
+        collection
+    end
+
+    def sum_multiples(limit)
+        collect_multiples(limit).sum
+    end
+
+end
